04649ae0681c719a1a3df27bbd2cb563b4b8417a
natalia <nataliav@gmail.com>
2019-04-27 20:25:10 -0400

    Updated React to latest

--- app/containers/Shortcode.jsx
+++ app/containers/Shortcode.jsx
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-export default class Shortcode extends Component {
-  render() {
-    return (
-      <div>
-        <h1>WP Reactivate Frontend</h1>
-        <p>Title: {this.props.wpObject.title}</p>
-      </div>
-    );
-  }
-}
+const Shortcode = () => (
+  <div>
+    <h1>WP Reactivate Frontend</h1>
+    <p>Title: {this.props.wpObject.title}</p>
+  </div>
+);
 
 Shortcode.propTypes = {
   wpObject: PropTypes.object
 };
+
+export default Shortcode;
